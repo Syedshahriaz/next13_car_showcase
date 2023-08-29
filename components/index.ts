b257f57b1439ca4a88345de31efdc097d4088df9
unknown <shahriaz2012@gmail.com>
2023-08-30 01:44:02 +0600

    init project

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import Footer from "./Footer";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    ShowMore
+}
